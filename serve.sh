8408d1d153dc67225cd583921010102dc89b4a82
hudidit <huyuexyz@gmail.com>
2019-01-28 00:07:27 +0800

    server runs in background process

--- serve.sh
+++ serve.sh
@@ -3,4 +3,4 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 echo -e '${YELLOW}Starting static server at port 443'
-http-server ./build -p 443 -S -C ../cert.pem -K ../privkey.pem
+nohup http-server ./build -p 443 -S -C ../cert.pem -K ../privkey.pem &
