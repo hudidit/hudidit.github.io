d1ad277e07866d2566245c84c1cd1d2251c7546e
hudidit <huyuexyz@gmail.com>
2019-01-27 23:59:08 +0800

    add color to shell output

--- serve.sh
+++ serve.sh
@@ -1,3 +1,6 @@
-npm install
-npm run build
+# 启动静态服务器
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e '${YELLOW}Starting static server at port 443'
 http-server ./build -p 443 -S -C ../cert.pem -K ../privkey.pem
--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 # 安装博客
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
-##
 ## 把该文件拷贝到服务器上运行
-
+echo -e '${YELLOW}Cloning files from github'
 git clone https://github.com/hudidit/hudidit.github.io.git
 
 cd hudidit.github.io
 
+echo -e '${YELLOW}Installing dependencies'
 npm install
 
+echo -e '${YELLOW}Building'
 npm run build
 
+# 启动静态资源服务器
 /bin/bash ./serve.sh
