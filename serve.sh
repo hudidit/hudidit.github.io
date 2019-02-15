3f6e54cdde6813ef82b6973c86b438821d658b01
hudidit <huyuexyz@gmail.com>
2019-02-16 01:21:17 +0800

    更新 bash 脚本

--- serve.sh
+++
@@ -1,6 +0,0 @@
-# 启动静态服务器
-YELLOW='\033[1;33m'
-NC='\033[0m'
-
-echo -e '${YELLOW}Starting static server at port 443'
-nohup http-server ./build -p 443 -S -C ../cert.pem -K ../privkey.pem &
--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 # 安装博客
+## 把该文件拷贝到服务器上运行
+
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-## 把该文件拷贝到服务器上运行
 echo -e '${YELLOW}Cloning files from github'
-git clone https://github.com/hudidit/hudidit.github.io.git
+git clone https://github.com/hudidit/hudidit.github.io.git --branch next next-blog
 
-cd hudidit.github.io
+cd next-blog
 
 echo -e '${YELLOW}Installing dependencies'
 npm install
 
-echo -e '${YELLOW}Building'
-npm run build
+echo -e '${YELLOW}Exporting HTML'
+npm run export
 
 # 启动静态资源服务器
-/bin/bash ./serve.sh
+# 确保上级目录存在 cert.pem 和 privkey.pem
+echo -e '${YELLOW}Starting static server at port 443'
+nohup http-server ./out -p 443 -S -C ../cert.pem -K ../privkey.pem &
