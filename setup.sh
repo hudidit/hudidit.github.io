71161f4e1de1a4a06a02743e89c8f12a240f9f23
hudidit <huyuexyz@gmail.com>
2019-03-02 20:01:56 +0800

    update readme. remove colors in setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,21 @@
 # 安装博客
 ## 把该文件拷贝到服务器上运行
-YELLOW='\033[1;33m'
-NC='\033[0m'
 
-echo -e '${YELLOW}Cloning files from github'
+echo -e 'Cloning files from github'
 git clone https://github.com/hudidit/hudidit.github.io.git --branch next next-blog
 
 cd next-blog
 
-echo -e '${YELLOW}Installing dependencies'
+echo -e 'Installing dependencies'
 npm install
 
-echo -e '${YELLOW}Building'
+echo -e 'Building'
 npm run build
 
-echo -e '${YELLOW}Exporting HTML'
+echo -e 'Exporting HTML'
 npm run export
 
 # 启动静态资源服务器
 # 确保上级目录存在 cert.pem 和 privkey.pem
-echo -e '${YELLOW}Starting static server at port 443'
+echo -e 'Starting static server at port 443'
 nohup http-server ./out -p 443 -S -C ../cert.pem -K ../privkey.pem &
